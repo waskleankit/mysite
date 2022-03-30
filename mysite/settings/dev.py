3279bac90827a3c5072a1b0a0e7d0a8b698492fd
waskleankit <ankitwaskle143@gmail.com>
2022-03-30 18:29:30 +0530

    database and requirement

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -1,48 +1,48 @@
-from .base import *
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = False
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g&nxd2pp%!$-6ftl#*hok9y0@lp^tou^yx3@=plrz62k2)708@'
-
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
-
-INSTALLED_APPS = INSTALLED_APPS + [
-    # ...
-    "debug_toolbar",
-    "django_extensions",
-    "wagtail.contrib.styleguide",
-    # ...
-]
-
-MIDDLEWARE += [
-    # ...
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-    # ...
-]
-
-INTERNAL_IPS = [
-    # ...
-    "127.0.0.1",
-    "172.17.0.1"
-    # ...
-]
-
-CACHES = {
-    "default":{
-        "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
-        "LOCATION": "\\Users\B&B\PycharmProjects\pythonProject\mysite\cache"
-    }
-}
-
-
-try:
-    from .local import *
-except ImportError:
-    pass
+# from .base import *
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+# # DEBUG = False
+#
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'g&nxd2pp%!$-6ftl#*hok9y0@lp^tou^yx3@=plrz62k2)708@'
+#
+# # SECURITY WARNING: define the correct hosts in production!
+# ALLOWED_HOSTS = ['*']
+#
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#
+#
+# INSTALLED_APPS = INSTALLED_APPS + [
+#     # ...
+#     "debug_toolbar",
+#     "django_extensions",
+#     "wagtail.contrib.styleguide",
+#     # ...
+# ]
+#
+# MIDDLEWARE += [
+#     # ...
+#     "debug_toolbar.middleware.DebugToolbarMiddleware",
+#     # ...
+# ]
+#
+# INTERNAL_IPS = [
+#     # ...
+#     "127.0.0.1",
+#     "172.17.0.1"
+#     # ...
+# ]
+#
+# CACHES = {
+#     "default":{
+#         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
+#         "LOCATION": "\\Users\B&B\PycharmProjects\pythonProject\mysite\cache"
+#     }
+# }
+#
+#
+# try:
+#     from .local import *
+# except ImportError:
+#     pass
